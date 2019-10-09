a713404f88a99f475e444c11f293ce9df1cc706f
Your Name <you@example.com>
2019-10-09 12:05:22 -0700

    SCHEME MODEL                                Wrote everything out now I just need to test it in my postman

---
+++ schemes/scheme-model.js
@@ -0,0 +1,51 @@
+const db = require('../data/seeds/01-schemes')
+
+module.exports = {
+    find,
+    findByID,
+    findSteps,
+    add,
+    update,
+    remove
+};
+
+function find(){
+    return db('scheme')
+}
+
+function findByID(id){
+    return db('scheme')
+    .where({ id }).first();
+}
+
+function findSteps(id){
+    return db('scheme as s')
+    .join('steps as st', 'st.scheme_id', 's.id')
+    .select('s.id', 's.scheme_name', 'st.step_number', 'st.instructions')
+    .where({scheme_name: scheme});
+}
+
+function add(scheme){
+    return db('scheme').insert(scheme, 'id' )
+    .then(([id]) => {
+        return findByID(id)
+    });
+}
+
+async function update(changes, id){
+    await db('scheme')
+    .where({id})
+    .update(changes);
+
+    return findByID(id);
+}
+
+async function remove(id){
+    const removed = await findByID(id);
+
+    await db('scheme')
+    .where({id})
+    .del();
+
+    return removed;
+}
